eed936964996e02b6282546b3838aee61b38ada7
Arvind iyer <ayalurarvind@gmail.com>
2016-03-24 14:59:03 +0530

    Made few changes .

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
  * Date: 23-Mar-16
  * Time: 12:58 PM
  */
+
 session_start();
 include_once("php/functions.php");
 ?>
@@ -25,9 +26,40 @@ include_once("php/functions.php");
     <p class="white-text">Presents</p>
     <img src="img/wall1.png" width="40%">
 </div>
+<?php
+if (!isset($_SESSION['player1'])) {
+    echo "Error";
+} else {
+    //db_connect();
+    $connect = mysql_connect("localhost", "root", "");
+    $db = mysql_select_db("udan_vsm", $connect);
+    $status = "SELECT status FROM status";
+    $result = mysql_query($status);
+    $status = 0;
+    if (!$result) {
+        echo "Could not execute query.";
+    } else {
+        $st = mysql_fetch_array($result);
+        $status = $st["status"];
+    }
+    if ($status == 0) {
+        echo "Welcome";
+        ?>
+        <script type="text/javascript">
+            login = document.getElementById('login');
+            text = document.getElementById('text');
+            login.addClass("show");
+            text.addClass("hide");
+        </script>
+    <?php
+    } else {
+        echo "Error";
+    }
+}
+?>
 <div class="bottom-sheet login center">
-    <button class="btn btn-flat blue waves-effect hide">Login</button>
-    <p class="white-text show">Game yet to start</p>
+    <button class="btn btn-flat blue waves-effect hide" id="login">Login</button>
+    <p class="white-text show" id="text">Game yet to start</p>
 </div>
 
 
@@ -35,7 +67,6 @@ include_once("php/functions.php");
     <div class="footer-copyright">
         <div class="container">
             <p class="center white-text">Made by <a class="orange-text text-lighten-3" href="#">Team Udaan</a>
-
             <p class="center white-text">Optimized by <a class="orange-text text-lighten-3" href="#">Arvind iyer</a>
             <p>
         </div>
--- php/functions.php
+++ php/functions.php
@@ -15,3 +15,4 @@ function db_connect()
 }
 
 ?>
+
